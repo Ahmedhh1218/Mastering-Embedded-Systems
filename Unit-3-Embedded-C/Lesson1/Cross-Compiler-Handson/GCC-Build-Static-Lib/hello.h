1868aff1ef4828350adb06ce76f3a45195d54c6a
Ahmed Hassabou <ahmed_hhassabou@outlook.com>
2024-09-17 03:26:12 +0300

    Unit3-Lesson1-Assignment-Commit

---
+++ Unit-3-Embedded-C/Lesson1/Cross-Compiler-Handson/GCC-Build-Static-Lib/hello.h
@@ -0,0 +1,8 @@
+#ifndef HELLO_H_
+#define HELLO_H_
+
+#include <stdio.h>
+
+void hello_world();
+
+#endif // HELLO_H_
---
+++ Unit-3-Embedded-C/Lesson1/Cross-Compiler-Handson/GCC-Build/hello.s
@@ -0,0 +1,27 @@
+	.file	"hello.c"
+	.section .rdata,"dr"
+LC0:
+	.ascii "Hello, World!\0"
+	.text
+	.globl	_hello_world
+	.def	_hello_world;	.scl	2;	.type	32;	.endef
+_hello_world:
+LFB10:
+	.cfi_startproc
+	pushl	%ebp
+	.cfi_def_cfa_offset 8
+	.cfi_offset 5, -8
+	movl	%esp, %ebp
+	.cfi_def_cfa_register 5
+	subl	$24, %esp
+	movl	$LC0, (%esp)
+	call	_puts
+	nop
+	leave
+	.cfi_restore 5
+	.cfi_def_cfa 4, 4
+	ret
+	.cfi_endproc
+LFE10:
+	.ident	"GCC: (MinGW.org GCC-6.3.0-1) 6.3.0"
+	.def	_puts;	.scl	2;	.type	32;	.endef
